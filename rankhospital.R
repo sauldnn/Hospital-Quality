faa58c01395f894fbe1c07278957c7b5e4a1b6c4
sauldnn <saul.dnn7@gmail.com>
2021-02-25 20:50:35 -0600

    ad headers and save memory

--- rankhospital.R
+++ rankhospital.R
@@ -1,11 +1,11 @@
-rankhospital <-function(state, outname, num="best"){
+  rankhospital <-function(state, outname, num="best"){
   #first capital letter and concatenate with a dot.
   col_outname <- gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", perl=TRUE) %>% 
     gsub(" ", ".") %>% 
     paste("Hospital.30.Day.Death..Mortality..Rates.from", sep=".")
   
   #check validation
-  X_full <-read.csv("outcome-of-care-measures.csv")
+  X_full <-read.csv("outcome-of-care-measures.csv", colClasses = "character", header=TRUE)
   
   if ((state %in% X_full[[7]])==FALSE){
     stop("invalid state")
@@ -16,30 +16,22 @@ rankhospital <-function(state, outname, num="best"){
   #building
   X <- X_full[X_full$State == State, ]
   X <- X[with(X, order(X[[col_outname]]))]
+  X[, col_outname] <- as.numeric(X[, col_outname])
+  X <- X[!is.na(X[[col_outname]])] #remove NA
+  
   
   #take cases and calculate the ratio...
   if (num=="best"){
-    ratio <-X[col_outname, 1]  #like random element of "num" rank position 
-    x <- sort(X[which(X$Hospital.Name == ratio), ])  #then sort (alpha) and ...
-    x[1]                       #take the first element.
+    num <- 1                  #take the first element.
   }
   else if (num="worst"){     #Repeat for the "num" rank
-    ratio <-X[col_outname, -1]
-    x <- sort(X[which(X$Hospital.Name == ratio), ])
-    x[-1]
+    num <- length(X[[col_outname]])
   }
-  else if (is.numeric(num)){
-    if(length(X$Hospital)<num){
-      stop(NA)
-    }
-    else{
-      ratio <-X[col_outname, num]
-      x <- sort(X[which(X$Hospital.Name == ratio), ])
-      x[num]
-    }
-  }
-  else{
-    stop("Error")
-  }
-  
+  else (is.numeric(num)){
+    if(length(X$Hospital)<num)
+      stop(NA)}
+  ratio <-X[num, col_outname]  #like random element of "num" rank position 
+  X <- X[which(X[[col_outname]] == ratio), ]  
+  X <- sort(X$Hospital.Name) #then sort (alpha) and ...
+  X[1]
 }
